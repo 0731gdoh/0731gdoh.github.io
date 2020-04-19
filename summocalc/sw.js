7afc2974444f36530c53fbca769c802d18ae4f7e
0731gdoh <62599624+0731gdoh@users.noreply.github.com>
2020-04-19 23:47:52 +0900

    Add files via upload

--- summocalc/sw.js
+++ summocalc/sw.js
@@ -1,3 +1,34 @@
-self.addEventListener("fetch", function(event){
+var CACHE_NAME = "cache-20200419-1";
+var urlsToCache = [
+  ".",
+  "index.html",
+  "js/summocalc.js",
+  "js/fraction.js",
+  "js/listitem.js",
+  "js/effect.js",
+  "js/card.js",
+  "js/ar.js",
+  "js/codec.js",
+  "js/calc.js",
+  "css/style.css"
+];
 
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE_NAME).then(function(cache){
+    return cache.addAll(urlsToCache);
+  }));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keyList){
+    return Promise.all(keyList.map(function(name){
+      if(CACHE_NAME.indexOf(name) === -1) return caches.delete(name);
+    }));
+  }));
+});
+
+self.addEventListener("fetch", function(e){
+  e.respondWith(caches.match(e.request).then(function(response){
+    return response || fetch(e.request);
+  }));
 });
