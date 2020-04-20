1018f2542d5f01be1041b6bc47b44722689756ba
0731gdoh <62599624+0731gdoh@users.noreply.github.com>
2020-04-20 18:31:32 +0900

    Cache test (#20)
    * Create style.css
    
    * Add files via upload

---
+++ test/sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = "test-cache-1";
+var urlsToCache = [
+  ".",
+  "index.html",
+  "style.css"
+];
+
+function message(txt){
+  clients.matchAll().then(function(list){
+    list.forEach(function(c){
+      c.postMessage(txt);
+    });
+  });
+}
+
+self.addEventListener("install", function(e){
+  message("install event " + CACHE_NAME);
+  e.waitUntil(caches.open(CACHE_NAME).then(function(cache){
+    return cache.addAll(urlsToCache);
+  }));
+});
+
+self.addEventListener("activate", function(e){
+  message("activate event " + CACHE_NAME);
+  e.waitUntil(caches.keys().then(function(keyList){
+    return Promise.all(keyList.map(function(name){
+      if(CACHE_NAME.indexOf(name) === -1) return caches.delete(name);
+    }));
+  }));
+});
+
+self.addEventListener("fetch", function(e){
+  message("fetch event " + e.request);
+  e.respondWith(caches.match(e.request).then(function(response){
+    return response || fetch(e.request);
+  }));
+});
