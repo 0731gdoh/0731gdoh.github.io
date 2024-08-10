a24820aeb633108492f5a437ad60d1caf514a55e
0731gdoh <62599624+0731gdoh@users.noreply.github.com>
2024-08-10 19:51:35 +0900

    Update data (#331)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- summocalc/js/variant.js
+++ summocalc/js/variant.js
@@ -96,6 +96,7 @@ var VARIANT = Variant.createList(
   ,"2024"
   ,["バレンタイン・クロスロード", "バレ24/Valentine24"]
   ,["本編第15章/Main Story Chapter 15", "15章24/Ch.1524"]
+  ,["ファビュラス・サマーホスト", "ホスト24/Host24"]
   ,"スロット/Slot"
   ,["[1枚目]/[1st Variant]", , 1]
   ,["[2枚目]/[2nd Variant]", , 2]
